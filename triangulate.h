02c8c6068ba4dd20783470c4117b211f0e1d7c76
Cameron Lowell Palmer <cameron.palmer@gmail.com>
2014-10-05 09:09:29 +0200

    Removed unused variables and cleaned up some initialization warnings

--- triangulate.h
+++ triangulate.h
@@ -4,61 +4,62 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 typedef struct {
-  double x, y;
+    double x, y;
 } point_t, vector_t;
 
 
 /* Segment attributes */
 
-typedef struct {	
-  point_t v0, v1;		/* two endpoints */
-  int is_inserted;		/* inserted in trapezoidation yet ? */
-  int root0, root1;		/* root nodes in Q */
-  int next;			/* Next logical segment */
-  int prev;			/* Previous segment */
+typedef struct {
+    point_t v0, v1;		/* two endpoints */
+    int is_inserted;		/* inserted in trapezoidation yet ? */
+    int root0, root1;		/* root nodes in Q */
+    int next;			/* Next logical segment */
+    int prev;			/* Previous segment */
 } segment_t;
 
 
 /* Trapezoid attributes */
 
 typedef struct {
-  int lseg, rseg;		/* two adjoining segments */
-  point_t hi, lo;		/* max/min y-values */
-  int u0, u1;
-  int d0, d1;
-  int sink;			/* pointer to corresponding in Q */
-  int usave, uside;		/* I forgot what this means */
-  int state;
+    int lseg, rseg;		/* two adjoining segments */
+    point_t hi, lo;		/* max/min y-values */
+    int u0, u1;
+    int d0, d1;
+    int sink;			/* pointer to corresponding in Q */
+    int usave, uside;		/* I forgot what this means */
+    int state;
 } trap_t;
 
 
 /* Node attributes for every node in the query structure */
 
 typedef struct {
-  int nodetype;			/* Y-node or S-node */
-  int segnum;
-  point_t yval;
-  int trnum;
-  int parent;			/* doubly linked DAG */
-  int left, right;		/* children */
+    int nodetype;			/* Y-node or S-node */
+    int segnum;
+    point_t yval;
+    int trnum;
+    int parent;			/* doubly linked DAG */
+    int left, right;		/* children */
 } node_t;
 
 
 typedef struct {
-  int vnum;
-  int next;			/* Circularly linked list  */
-  int prev;			/* describing the monotone */
-  int marked;			/* polygon */
-} monchain_t;			
+    int vnum;
+    int next;			/* Circularly linked list  */
+    int prev;			/* describing the monotone */
+    int marked;			/* polygon */
+} monchain_t;
 
 
 typedef struct {
-  point_t pt;
-  int vnext[4];			/* next vertices for the 4 chains */
-  int vpos[4];			/* position of v in the 4 chains */
-  int nextfree;
+    point_t pt;
+    int vnext[4];			/* next vertices for the 4 chains */
+    int vpos[4];			/* position of v in the 4 chains */
+    int nextfree;
 } vertexchain_t;
 
 
@@ -70,10 +71,10 @@ typedef struct {
 
 
 #define SEGSIZE 200		/* max# of segments. Determines how */
-				/* many points can be specified as */
-				/* input. If your datasets have large */
-				/* number of points, increase this */
-				/* value accordingly. */
+/* many points can be specified as */
+/* input. If your datasets have large */
+/* number of points, increase this */
+/* value accordingly. */
 
 #define QSIZE   8*SEGSIZE	/* maximum table sizes */
 #define TRSIZE  4*SEGSIZE	/* max# trapezoids */
@@ -83,16 +84,14 @@ typedef struct {
 #define FALSE 0
 
 
-#define FIRSTPT 1		/* checking whether pt. is inserted */ 
+#define FIRSTPT 1		/* checking whether pt. is inserted */
 #define LASTPT  2
 
-
-#define INFINITY 1<<30
 #define C_EPS 1.0e-7		/* tolerance value: Used for making */
-				/* all decisions about collinearity or */
-				/* left/right of segment. Decrease */
-				/* this value if the input points are */
-				/* spaced very close together */
+/* all decisions about collinearity or */
+/* left/right of segment. Decrease */
+/* this value if the input points are */
+/* spaced very close together */
 
 
 #define S_LEFT 1		/* for merge-direction */
@@ -110,7 +109,7 @@ typedef struct {
 #define SP_2UP_RIGHT   5
 #define SP_2DN_LEFT    6
 #define SP_2DN_RIGHT   7
-#define SP_NOSPLIT    -1	
+#define SP_NOSPLIT    -1
 
 #define TR_FROM_UP 1		/* for traverse-direction */
 #define TR_FROM_DN 2
@@ -123,7 +122,7 @@ typedef struct {
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
 #define CROSS(v0, v1, v2) (((v1).x - (v0).x)*((v2).y - (v0).y) - \
-			   ((v1).y - (v0).y)*((v2).x - (v0).x))
+((v1).y - (v0).y)*((v2).x - (v0).x))
 
 #define DOT(v0, v1) ((v0).x * (v1).x + (v0).y * (v1).y)
 
